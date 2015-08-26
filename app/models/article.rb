f4f08d33b0974f609f09afb68665e1b4a1393c66
Flipez <github@flipez.net>
2015-08-26 09:36:38 +0200

    update articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ActiveRecord::Base
+
+  belongs_to :user
+
+  validates :title, presence: true, length: { in: 5..25 }
+  validates :text, presence: true
+  validates :user, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725124639) do
+ActiveRecord::Schema.define(version: 20150826070925) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
