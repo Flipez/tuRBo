59c1edeb71c445ccb267072420cbdb238b8b5afd
Flipez <github@flipez.net>
2015-09-02 07:23:58 +0000

    add user/about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  
   root 'welcome#index'
+  
+  get '/about' => 'about#index'
   
   resources :articles
   resources :categories
+  resources :users
 end
