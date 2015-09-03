2d44932a0377183e98c4fe48a6299862acbe3ef8
Flipez <github@flipez.net>
2015-09-03 07:27:49 +0000

    add userhandling to adminpanel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,35 @@
 class AdminController < ApplicationController
+  
+  before_filter :set_up_variables
+
+  def set_up_variables
+    @articles = Article.all
+    @users = User.all
+    @categories = Category.all
+  end
+  
   def index
   end
+
+  # Actions for User handling in admin panel
+  def user_index
+  end
+  
+  def user_new
+  end
+  
+  def user_create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to :back
+  end
+
+  def user_show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  get '/admin' => 'admin#index'
 
   get 'welcome/index'
 
@@ -9,5 +9,12 @@ Rails.application.routes.draw do
   
   resources :articles
   resources :categories
+
   resources :users
+
+  get '/admin/users' => 'admin#user_index'
+  get '/admin/users/new' => 'admin#user_new'
+  post '/admin/users/new' => 'admin#user_create'
+
+  get '/admin/users/:id' => 'admin#user_show'
 end
