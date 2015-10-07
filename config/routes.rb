568112f55fa0f550b10db36a114eafa53756e613
Nyomancer <nyo@nyo-node.net>
2015-10-07 08:17:37 +0200

    added session based authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   get 'admin/index'
 
   get 'welcome/index'
