0bcff869cd822610a439483e7900deddbcebe60f
Flipez <github@flipez.net>
2015-10-08 07:41:07 +0200

    update views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_filter :set_variables
+
+
+  def set_variables
+    @categories = Category.all
+  end
 end
