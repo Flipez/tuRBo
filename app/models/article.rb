5c61e758f596a61b027452dbe96dc1a5b1bf4ab0
Flipez <github@flipez.net>
2015-12-05 10:58:26 +0000

    update category handling in frontend

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ActiveRecord::Base
   
   acts_as_taggable
 
-  has_and_belongs_to_many :categories
+  belongs_to :category
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :text, presence: true
