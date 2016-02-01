973bd63794df18654b711ed86ad9d70025d0da52
Flipez <github@flipez.net>
2016-02-01 17:24:44 +0000

    split view in partials, update settings
    Signed-off-by: Flipez <github@flipez.net>

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -8,6 +8,9 @@ MySettings.defaults[:steam]    = 'https://steamcommunity.com/id/flipez'
 MySettings.defaults[:flickr]   = 'https://flickr.com/photos/Flipez'
 MySettings.defaults[:mail]     = 'mailto:rm@flipez.net'
 
+MySettings.defaults[:recaptcha_pubkey] = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
+MySettings.defaults[:recaptcha_privkey] = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
+
 MySettings.defaults[:welcome]  = 'Wenn ich dich mag, fühl\' dich gegrüßt.'
 MySettings.defaults[:about]    = 'Der Blog wurde mit Ruby on Rails erstellt, mit SQLite gefüttert und mit Puma zum laufen gebracht. Wenn Du uns helfen möchtest kannst du gern unserem GitHub Repository beitragen.'
 
