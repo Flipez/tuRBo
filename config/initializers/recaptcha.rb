0c29c00e031cfc0736ee4bf1ffa2373be258dbf6
Flipez <github@flipez.net>
2016-02-01 17:25:32 +0000

    switch to new settings
    Signed-off-by: Flipez <github@flipez.net>

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = Settings.recaptcha.pubkey
-  config.private_key = Settings.recaptcha.privkey
+  config.public_key  = MySettings.recaptcha_pubkey
+  config.private_key = MySettings.recaptcha_privkey
   #config.public_key  = '6LfcrRITAAAAAC8njz5T8s72B37YTuchbZTAY5wL'
   #config.private_key = '6LfcrRITAAAAAB2uSz85Q_qL_e9na_MephEqPmax'
 end
