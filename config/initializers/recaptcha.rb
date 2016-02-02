de9944e2d26a729631118b3d0d1707cdd7f0ad6e
Flipez <github@flipez.net>
2016-02-02 10:08:30 +0000

    fix crash without db
    Signed-off-by: Flipez <github@flipez.net>

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = MySettings.recaptcha_pubkey
-  config.private_key = MySettings.recaptcha_privkey
-  #config.public_key  = '6LfcrRITAAAAAC8njz5T8s72B37YTuchbZTAY5wL'
-  #config.private_key = '6LfcrRITAAAAAB2uSz85Q_qL_e9na_MephEqPmax'
+  config.public_key  = MySettings.recaptcha_pubkey rescue nil
+  config.private_key = MySettings.recaptcha_privkey rescue nil
 end
