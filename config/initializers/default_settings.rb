81ce5a7f65d53b4c1588e0485d72144d41eed37b
Flipez <github@flipez.net>
2016-02-02 19:11:24 +0000

    update defaults
    Signed-off-by: Flipez <github@flipez.net>

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,12 +1,14 @@
-MySettings.defaults[:title]    = 'super dupa blog'
-MySettings.defaults[:subtitle] = 'Linux & IT-Fan, Nerd, Supertüp'
+MySettings.defaults[:title]             = 'tuRBo'
+MySettings.defaults[:subtitle]          = 'the ultimate ruby blog :o'
 
 MySettings.defaults[:rss]      = "/feed?format=rss"
 
-MySettings.defaults[:recaptcha_pubkey] = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
+MySettings.defaults[:rss]               = "/feed?format=rss"
+
+MySettings.defaults[:recaptcha_pubkey]  = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
 MySettings.defaults[:recaptcha_privkey] = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
 
-MySettings.defaults[:welcome]  = 'Wenn ich dich mag, fühl\' dich gegrüßt.'
-MySettings.defaults[:about]    = 'Der Blog wurde mit Ruby on Rails erstellt, mit SQLite gefüttert und mit Puma zum laufen gebracht. Wenn Du uns helfen möchtest kannst du gern unserem GitHub Repository beitragen.'
+MySettings.defaults[:welcome]           = 'Say hi to your friends o/'
+MySettings.defaults[:about]             = 'Tell something about you. Maybe how you defeated the dragon.'
 
-MySettings.defaults[:version]    = '0.9.6b'
+MySettings.defaults[:version]           = '0.9.6b'
