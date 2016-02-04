f41c09dbe0554ef763c4ae27579e785e020f7e15
Flipez <github@flipez.net>
2016-02-04 10:51:11 +0100

    rescue if table is missing while migrating first
    Signed-off-by: Flipez <github@flipez.net>

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,15 +1,18 @@
-MySettings.defaults[:title]             = 'tuRBo'
-MySettings.defaults[:subtitle]          = 'the ultimate ruby blog :o'
-
-MySettings.defaults[:available_locales] = [:de, :en]
-Rails.application.config.i18n.default_locale = MySettings.locale || :de
-
-MySettings.defaults[:rss]               = "/feed?format=rss"
-
-MySettings.defaults[:recaptcha_pubkey]  = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
-MySettings.defaults[:recaptcha_privkey] = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
-
-MySettings.defaults[:welcome]           = 'Say hi to your friends o/'
-MySettings.defaults[:about]             = 'Tell something about you. Maybe how you defeated the dragon.'
-
-MySettings.defaults[:version]           = '0.9.6b'
+begin
+  MySettings.defaults[:title]             = 'tuRBo'
+  MySettings.defaults[:subtitle]          = 'the ultimate ruby blog :o'
+  
+  MySettings.defaults[:available_locales] = [:de, :en]
+  Rails.application.config.i18n.default_locale = MySettings.locale || :de
+  
+  MySettings.defaults[:rss]               = "/feed?format=rss"
+  
+  MySettings.defaults[:recaptcha_pubkey]  = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
+  MySettings.defaults[:recaptcha_privkey] = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
+  
+  MySettings.defaults[:welcome]           = 'Say hi to your friends o/'
+  MySettings.defaults[:about]             = 'Tell something about you. Maybe how you defeated the dragon.'
+  
+  MySettings.defaults[:version]           = '0.9.6b'
+rescue
+end
